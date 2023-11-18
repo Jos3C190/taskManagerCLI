376b475e62646e2726ff842ccc7b77f20647fe10
Jose Carlos <josecarloslopezm17@gmail.com>
2023-11-18 10:05:42 -0600

    -Added date management using chrono

--- src/task.rs
+++ src/task.rs
@@ -1,20 +1,28 @@
+use chrono::Local;
+
 pub struct Task {
     pub name: String,
     pub description: String,
     pub completed: bool,
+    pub created_at: chrono::NaiveDateTime, 
+    pub completed_at: Option<chrono::NaiveDateTime>,
 }
 
 impl Task {
     pub fn new(name: String, description: String) -> Task {
+        let created_at = Local::now().naive_local();
 
         Task {
             name,
             description,
             completed: false,
+            created_at,
+            completed_at: None,
         }
     }
 
     pub fn mark_as_completed(&mut self) {
         self.completed = true;
+        self.completed_at = Some(Local::now().naive_local());
     }
 }
